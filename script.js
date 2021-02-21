fb96d54b37443c99f5673b2864ebfc6479e045a2
Erik Moratori <emoratori@yahoo.com.br>
2021-02-21 17:29:06 -0300

    Defnindo retorno da tela

--- script.js
+++ script.js
@@ -20,14 +20,27 @@ function criaCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criaCobrinha();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "rigth") snakeX += box;
+    if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -=box;
     if(direction == "down") snakeY += box;
